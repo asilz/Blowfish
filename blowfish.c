82426352a048dd826f527096822f2c155b013435
Asil Zogby <asilz@stud.ntnu.no>
2024-02-09 11:45:37 +0100

    Refactor Code
    Revised init function to allow any key length, thus allowing the removal of extendKey function. Add a cryptBlock function to minimize code repetition between decryptBlock and encryptBlock. Also added 3 functions to minimize for loops in main function

--- blowfish.c
+++ blowfish.c
@@ -1,39 +1,78 @@
 #include "blowfish.h"
-
 // g++ main.cpp -o out // -lgmpxx -lgmp
 
-#define textLength 2
-
-uint64_t F(uint32_t leftHalf)
+static uint32_t F(uint32_t leftHalf)
 {
     uint8_t box0 = (uint8_t)(leftHalf >> 24);
     uint8_t box1 = (uint8_t)(leftHalf >> 16);
     uint8_t box2 = (uint8_t)(leftHalf >> 8);
     uint8_t box3 = (uint8_t)leftHalf;
 
-    uint32_t a = sbox0[box0];
-    uint32_t b = sbox1[box1];
-    uint32_t c = sbox2[box2];
-    uint32_t d = sbox3[box3];
+    uint32_t a = sbox[0][box0];
+    uint32_t b = sbox[1][box1];
+    uint32_t c = sbox[2][box2];
+    uint32_t d = sbox[3][box3];
 
-    uint64_t out;
-    out = a + b;
+    uint32_t out;
+    out = a + b; // Modulo not neccesary due to overflow being removed due to the size limit of uint32_t
     out = out ^ c;
     out = out + d;
 
     return out;
 }
 
+static void cryptBlock(uint64_t *block, bool encrypt)
+{
+    uint32_t rightHalf = (uint32_t)(*block & 0xFFFFFFFF);
+    uint32_t leftHalf = (uint32_t)(*block >> 32);
+    uint32_t temp;
+    int index;
+    for (int i = 0; i < pArrayLength - 2; ++i)
+    {
+        if (encrypt)
+        {
+            index = i;
+        }
+        else
+        {
+            index = pArrayLength - 1 - i;
+        }
+        leftHalf = leftHalf ^ pArray[index];
+        rightHalf = F(leftHalf) ^ rightHalf;
+        temp = leftHalf;
+        leftHalf = rightHalf;
+        rightHalf = temp;
+    }
+
+    temp = leftHalf;
+    leftHalf = rightHalf;
+    rightHalf = temp;
+    if (encrypt)
+    {
+        rightHalf = rightHalf ^ pArray[++index];
+        leftHalf = leftHalf ^ pArray[++index];
+    }
+    else
+    {
+        rightHalf = rightHalf ^ pArray[--index];
+        leftHalf = leftHalf ^ pArray[--index];
+    }
+
+    *block = ((uint64_t)leftHalf << 32) | rightHalf;
+}
+
 void encryptBlock(uint64_t *block)
 {
+    cryptBlock(block, true);
     // printf("Encrypting block\n");
+    /*
     uint32_t rightHalf = (uint32_t)(*block & 0xFFFFFFFF);
     uint32_t leftHalf = (uint32_t)(*block >> 32);
     uint32_t temp;
 
     for (int i = 0; i < pArrayLength - 2; ++i)
     {
-        leftHalf = leftHalf ^ pARRAY[i];
+        leftHalf = leftHalf ^ pArray[i];
         rightHalf = F(leftHalf) ^ rightHalf;
         temp = leftHalf;
         leftHalf = rightHalf;
@@ -44,22 +83,25 @@ void encryptBlock(uint64_t *block)
     leftHalf = rightHalf;
     rightHalf = temp;
 
-    rightHalf = rightHalf ^ pARRAY[16];
-    leftHalf = leftHalf ^ pARRAY[17];
+    rightHalf = rightHalf ^ pArray[16];
+    leftHalf = leftHalf ^ pArray[17];
 
     *block = ((uint64_t)leftHalf << 32) | rightHalf;
+    */
 }
 
 void decryptBlock(uint64_t *block)
 {
+    cryptBlock(block, false);
     // printf("Encrypting block\n");
+    /*
     uint32_t rightHalf = (uint32_t)(*block & 0xFFFFFFFF);
     uint32_t leftHalf = (uint32_t)(*block >> 32);
     uint32_t temp;
 
     for (int i = pArrayLength - 1; i > 1; --i)
     {
-        leftHalf = leftHalf ^ pARRAY[i];
+        leftHalf = leftHalf ^ pArray[i];
         rightHalf = F(leftHalf) ^ rightHalf;
         temp = leftHalf;
         leftHalf = rightHalf;
@@ -70,108 +112,95 @@ void decryptBlock(uint64_t *block)
     leftHalf = rightHalf;
     rightHalf = temp;
 
-    rightHalf = rightHalf ^ pARRAY[1];
-    leftHalf = leftHalf ^ pARRAY[0];
+    rightHalf = rightHalf ^ pArray[1];
+    leftHalf = leftHalf ^ pArray[0];
 
     *block = ((uint64_t)leftHalf << 32) | rightHalf;
+    */
 }
 
-void parrayInit(uint8_t *extendedKey)
+void encryptData(uint64_t *data, size_t dataLength)
 {
-    for (int i = 0; i < pArrayLength; ++i)
+    for (int i = 0; i < dataLength; ++i)
     {
-        // printf("%0x\n", ((uint32_t)((uint8_t)(pARRAY[i] >> 16) ^ extendedKey[(i * 4) + 2])) << 16);
-        pARRAY[i] = (pARRAY[i] & 0xFFFFFF00) | (uint32_t)((uint8_t)(pARRAY[i] >> 0) ^ extendedKey[(i * 4)]);
-        pARRAY[i] = (pARRAY[i] & 0xFFFF00FF) | ((uint32_t)((uint8_t)(pARRAY[i] >> 8) ^ extendedKey[(i * 4) + 1])) << 8;
-        pARRAY[i] = (pARRAY[i] & 0xFF00FFFF) | ((uint32_t)((uint8_t)(pARRAY[i] >> 16) ^ extendedKey[(i * 4) + 2])) << 16;
-        pARRAY[i] = (pARRAY[i] & 0x00FFFFFF) | ((uint32_t)((uint8_t)(pARRAY[i] >> 24) ^ extendedKey[(i * 4) + 3])) << 24;
+        encryptBlock(&data[i]);
     }
+}
 
+void decryptData(uint64_t *data, size_t dataLength)
+{
+    for (int i = 0; i < dataLength; ++i)
+    {
+        decryptBlock(&data[i]);
+    }
+}
+
+void initBlowfish(uint8_t *key, size_t keyLength)
+{
+    int keyIndex = 0;
+    for (int i = 0; i < pArrayLength * sizeof(uint32_t); ++i)
+    {
+        if (keyIndex >= keyLength)
+        {
+            keyIndex = 0;
+        }
+        pArray[i / 4] = (pArray[i / 4] & (uint32_t)(0xFFFFFFFF00FFFFFF >> (24 - (8 * (i % 4))))) | ((uint32_t)((uint8_t)(pArray[i / 4] >> (8 * (i % 4))) ^ key[keyIndex++])) << (8 * (i % 4));
+    }
     uint64_t block = 0x0000000000000000;
     for (int i = 0; i < pArrayLength; i += 2)
     {
-        // printf("pARRAY init encrypting\n");
         encryptBlock(&block);
         uint32_t rightHalf = (uint32_t)(block);
         uint32_t leftHalf = (uint32_t)(block >> 32);
-        pARRAY[i] = leftHalf;
-        pARRAY[i + 1] = rightHalf;
+        pArray[i] = leftHalf;
+        pArray[i + 1] = rightHalf;
     }
-    for (int i = 0; i < 256; i += 2)
+    for (int i = 0; i < 4; ++i)
     {
-        encryptBlock(&block);
-        uint32_t rightHalf = (uint32_t)(block);
-        uint32_t leftHalf = (uint32_t)(block >> 32);
-        sbox0[i] = leftHalf;
-        sbox0[i + 1] = rightHalf;
-    }
-    for (int i = 0; i < 256; i += 2)
-    {
-        encryptBlock(&block);
-        uint32_t rightHalf = (uint32_t)(block);
-        uint32_t leftHalf = (uint32_t)(block >> 32);
-        sbox1[i] = leftHalf;
-        sbox1[i + 1] = rightHalf;
-    }
-    for (int i = 0; i < 256; i += 2)
-    {
-        encryptBlock(&block);
-        uint32_t rightHalf = (uint32_t)(block);
-        uint32_t leftHalf = (uint32_t)(block >> 32);
-        sbox2[i] = leftHalf;
-        sbox2[i + 1] = rightHalf;
-    }
-    for (int i = 0; i < 256; i += 2)
-    {
-        // printf("sBox3 encrypting\n");
-        encryptBlock(&block);
-        uint32_t rightHalf = (uint32_t)(block);
-        uint32_t leftHalf = (uint32_t)(block >> 32);
-        sbox3[i] = leftHalf;
-        sbox3[i + 1] = rightHalf;
-    }
-}
-
-void extendKey(uint8_t *key, size_t keyLength, uint8_t *extendedKey)
-{
-    // printf("Extending Key\n");
-    size_t nKey = 0;
-    for (int i = 0; i < 18 * sizeof(uint32_t); ++i)
-    {
-        if (nKey == keyLength)
+        for (int j = 0; j < 256; j += 2)
         {
-            nKey = 0;
+            encryptBlock(&block);
+            uint32_t rightHalf = (uint32_t)(block);
+            uint32_t leftHalf = (uint32_t)(block >> 32);
+            sbox[i][j] = leftHalf;
+            sbox[i][j + 1] = rightHalf;
         }
-        extendedKey[i] = key[nKey];
-        ++nKey;
-        // printf("%x", extendedKey[i]);
     }
-    // printf("Extended Key\n");
 }
 
-#define keyLength 11
+void printData(uint64_t *data, size_t length)
+{
+    for (int i = 0; i < length; ++i)
+    {
+        printf("%0lX\n", data[i]);
+    }
+}
+
+void printText(uint64_t *data, size_t length)
+{
+    for (int i = 0; i < length; ++i)
+    {
+        for (int i = 0; i < sizeof(uint64_t); ++i)
+            printf("%c", (uint8_t)(data[i] >> 8 * (i % 4)));
+    }
+}
+
+#define keyLength 1
+#define dataLength 2
 
 int main()
 {
     // uint8_t set_key[24] = {0xF0, 0xE1, 0xD2, 0xC3, 0xB4, 0xA5, 0x96, 0x87,0x78, 0x69, 0x5A, 0x4B, 0x3C, 0x2D, 0x1E, 0x0F,0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77};
-    // uint8_t set_key[] = "\x96\xCA\x99\x9F\x8D\xDA\x9A\x87\xD7\xCD\xD9\xBB\x93\xD1\xBE\xC0\xD7\x91\x71\xDC\x9E\xD9\x8D\xD0\xD1\x8C\xD8\xC3\xA0\xB0\xC6\x95\xC3\x9C\x93\xBB\xCC\xCC\xA7\xD3\xB9\xD9\xD9\xD0\x8E\x93\xBE\xDA\xAE\xD1\x8D\x77\xD5\xD3\xA3";
+    // uint8_t set_key[] = "\x96\xCA\x99\x9F\x8D\xDA\x9A\x87\xD7\xCD\xD9\xBB\x93\xD1\xBE\xC0\xD7\x91\x71\xDC\x9E\xD9\x8D\xD0\xD1\x8C\xD8\xC3\xA0\xB0\xC6\x95\xC3\x9C\x93\xBB\xCC\xCC\xA7\xD3\xB9\xD9\xD9\xD0\x8E\x93\xBE\xDA\xAE\xD1\x8D\x77\xD5\xD3\xA3\x96\xCA\x99\x9F\x8D\xDA\x9A\x87\xD7\xCD\xD9\xBB\x93\xD1\xBE\xC0\xD7\x91\x71\xDC\x9E\xD9\x8D\xD0\xD1\x8C\xD8\xC3\xA0\xB0\xC6\x95\xC3\x9C\x93\xBB\xCC\xCC\xA7\xD3\xB9\xD9\xD9\xD0\x8E\x93\xBE\xDA\xAE\xD1\x8D\x77\xD5\xD3\xA3";
     // uint8_t set_key[] = "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF";
-    uint8_t set_key[] = "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF";
-    uint64_t plainText[2] = {0x0000000000000000, 0xFFFFFFFFFFFFFFFF};
-    uint8_t *key = set_key;
-    uint8_t *extendedKey = malloc(18 * sizeof(uint32_t));
-    extendKey(key, keyLength, extendedKey);
-    parrayInit(extendedKey);
-    printf("\nparray inited\n");
-    for (int i = 0; i < textLength; ++i)
-    {
-        printf("%lx\n", plainText[i]);
-        encryptBlock(&plainText[i]);
-        printf("%lx\n", plainText[i]);
-        decryptBlock(&plainText[i]);
-        printf("%lx\n", plainText[i]);
-    }
-
-    free(extendedKey);
+    // uint8_t set_key[] = "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF";
+    uint8_t key[] = "\xFF";
+    uint64_t plainText[dataLength] = {0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+    // extendKey(key, keyLength, extendedKey);
+    initBlowfish(key, keyLength);
+    encryptData(plainText, dataLength);
+    printData(plainText, dataLength);
+    decryptData(plainText, dataLength);
+    printData(plainText, dataLength);
     return 0;
 }
